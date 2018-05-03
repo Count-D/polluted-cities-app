e91011d4b57db37539c219ac3a242cf35ca05a84
Dejan Dabic <dejan.dabic023@gmail.com>
2018-05-03 20:30:43 +0200

    added map api key

--- src/components/map.js
+++ src/components/map.js
@@ -5,7 +5,7 @@ import MarkersAll from './markers';
 
 const Map = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyALJtFGGPPOowxAsox9ip3CMMrFAU1mZ0U&callback=initMap",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{  height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
